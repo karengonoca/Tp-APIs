6767fb75e06c3f9b6ab2bf4b3e5657680c9cd951
Karen Gonzalez <kagoncaoca@gmail.com>
2023-01-13 17:06:13 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const methodOverride = require('method-override');
 const app = express();
+const cors = require('cors');
 
 
 //Ejecuto el llamado a mis rutas
@@ -21,6 +22,7 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 //URL encode  - Para que nos pueda llegar la información desde el formulario al req.body
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 //Aquí estoy disponiendo la posibilidad para utilizar el seteo en los formularios para el usod e los metodos put ó delete
 app.use(methodOverride('_method'));
